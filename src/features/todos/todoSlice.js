8ad186fbde868291ff18e73d347f6a4a0abae79e
최성익 <sungik.dev@gmail.com>
2020-11-10 20:58:44 +0900

    Feat : 5장 기본 세팅

--- src/features/todos/todoSlice.js
+++ src/features/todos/todoSlice.js
@@ -8,6 +8,7 @@ function nextTodoId(todos) {
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
+      // Can return just the new todos array - no extra object around it
       return [
         ...state,
         {
@@ -19,12 +20,39 @@ export default function todosReducer(state = initialState, action) {
     }
     case 'todos/todoToggled': {
       return state.map((todo) => {
-        if (todo.id !== action.payload) return todo
+        if (todo.id !== action.payload) {
+          return todo
+        }
+
         return {
           ...todo,
           completed: !todo.completed,
         }
       })
+    }
+    case 'todos/colorSelected': {
+      const { color, todoId } = action.payload
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo
+        }
+
+        return {
+          ...todo,
+          color,
+        }
+      })
+    }
+    case 'todos/todoDeleted': {
+      return state.filter((todo) => todo.id !== action.payload)
+    }
+    case 'todos/allCompleted': {
+      return state.map((todo) => {
+        return { ...todo, completed: true }
+      })
+    }
+    case 'todos/completedCleared': {
+      return state.filter((todo) => !todo.completed)
     }
     default:
       return state
--- src/store.js
+++ src/store.js
@@ -3,9 +3,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
 import { print1, print2, print3 } from './exampleAddons/middleware'
 
-const composedEnhancer = composeWithDevTools(
-  applyMiddleware(print1, print2, print3)
-)
+const composedEnhancer = composeWithDevTools(applyMiddleware())
 
 const store = createStore(rootReducer, composedEnhancer)
 
